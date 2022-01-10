9621fb78fec74f720b5532dab4cf7ae69b0b9f15
tygrski <77369211+tygrski@users.noreply.github.com>
2022-01-10 14:25:05 -0600

    updated server.js

--- server/server.js
+++ server/server.js
@@ -30,13 +30,17 @@ startServer()
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+app.use(routes);
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
+
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
