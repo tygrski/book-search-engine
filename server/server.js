e890622fac01b1903fa4ae3ec1bc04e9c7eefc6d
tygrski <77369211+tygrski@users.noreply.github.com>
2022-01-11 12:37:52 -0600

    loading error

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const {typeDefs, resolvers} = require('./schemas');
 
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
+// const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -29,7 +29,7 @@ startServer()
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(routes);
+// app.use(routes);
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
@@ -40,7 +40,6 @@ if (process.env.NODE_ENV === 'production') {
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
-
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
